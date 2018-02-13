e993fa82624a22c815a214fc8efb90bb71fe897e
ESMAD01HWLAP001\VictorV <victor.valero@mgt-consulting.com>
2018-02-13 16:23:40 +0100

    HBaseStram

--- src/main/scala/HBase.scala
+++ src/main/scala/HBase.scala
@@ -1,31 +1,37 @@
-//import it.nerdammer.spark.hbase._
-//import org.apache.spark.SparkContext
-//import org.apache.spark.SparkConf
-//
-///**
-//  * Created by hkropp on 19/04/15.
-//  */
-//object HBase{
-//  //  org.apache.log4j.BasicConfigurator.configure()
-//  def main(args: Array[String])  {
-//
-//    val sparkConf = new SparkConf().setAppName("SanitasSpark")
-//    if (sys.env("ENTORNO") == "DESARROLLO") {
-//      sparkConf.setMaster("local[*]")
-//    }
-//
-//    sparkConf.set("spark.hbase.host", "sandbox.hortonworks.com:21000")
-//    val sc = new SparkContext(sparkConf)
-//
-//    val rdd = sc.parallelize(1 to 100)
-//      .map(i => (i.toString, i+1, "Hello"))
-//
-//    rdd.toHBaseTable("b")
-//      .toColumns("b")
-//      .inColumnFamily("b")
-//      .save()
-//
-//
-//
-//  }
-//}
+import org.apache.hadoop.hbase.{HBaseConfiguration,TableName}
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.client.Put
+import org.apache.hadoop.hbase.spark.HBaseContext
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.spark.HBaseRDDFunctions._
+
+import org.apache.spark.{SparkContext,SparkConf}
+
+object HBase{
+    def main(args: Array[String])  {
+
+
+    val sparkConf = new SparkConf().setAppName("HBaseDemo")
+    val sc = new SparkContext(sparkConf)
+    val conf = HBaseConfiguration.create()
+    val hbaseContext = new HBaseContext(sc, conf)
+
+    val rdd =  sc.parallelize(Array(
+      (Bytes.toBytes("row1"),
+        Array((Bytes.toBytes("colFamily)"), Bytes.toBytes("col1"), Bytes.toBytes("value1"))))))
+
+    rdd.hbaseForeachPartition(hbaseContext, (it, conn) => {
+    val bufferedMutator = conn.getBufferedMutator(TableName.valueOf("usuarios"))
+
+    it.foreach( putRecord => {
+      val put = new Put(putRecord._1)
+      putRecord._2.foreach((putValue) => put.addColumn(putValue._1, putValue._2, putValue._3))
+      bufferedMutator.mutate(put)
+      })
+      bufferedMutator.flush()
+      bufferedMutator.close()
+    })
+
+
+  }
+}
